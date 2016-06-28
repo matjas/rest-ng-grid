88cb05e806dc56b8b55ebb208b968e581db93d9d
Maciej Jaskula <mjaskula@windowslive.com>
2016-06-28 19:52:04 +0200

    demo - basic usage page

--- misc/demo/assets/app.js
+++ misc/demo/assets/app.js
@@ -1,4 +1,4 @@
-angular.module('restNgGrid.demo', ['ngMockE2E', 'ngResource', 'ngRoute','ui.bootstrap', 'restNgGrid']);
+angular.module('restNgGrid.demo', ['ngMockE2E', 'ngResource', 'ngRoute','ui.bootstrap', 'restNgGrid', 'plunker', 'bootstrapPrettify']);
 
 angular.module('restNgGrid.demo').
     config(['$locationProvider', '$routeProvider', function config ($locationProvider, $routeProvider){
@@ -15,3 +15,33 @@ angular.module('restNgGrid.demo').
         }).
         otherwise('/tutorial')
 }]);
+
+directive.ngHtmlWrapGrid = ['reindentCode', 'templateMerge', function(reindentCode, templateMerge) {
+    return {
+        compile: function(element, attr) {
+            var properties = {
+                    head: '',
+                    module: '',
+                    body: element.text()
+                },
+                html = "<!doctype html>\n<html ng-app{{module}}>\n  <head>\n{{head:4}}  </head>\n  <body>\n{{body:4}}  </body>\n</html>";
+
+            angular.forEach((attr.ngHtmlWrap || '').split(' '), function(dep) {
+                if (!dep) return;
+                dep = DEPENDENCIES[dep] || dep;
+
+                var ext = dep.split(/\./).pop();
+
+                if (ext == 'css') {
+                    properties.head += '<link rel="stylesheet" href="' + dep + '" type="text/css">\n';
+                } else if(ext == 'js') {
+                    properties.head += '<script src="' + dep + '"></script>\n';
+                } else {
+                    properties.module = '="' + dep + '"';
+                }
+            });
+
+            setHtmlIe8SafeWay(element, escape(templateMerge(html, properties)));
+        }
+    }
+}];
--- misc/demo/assets/controllers/basic-usage.js
+++ misc/demo/assets/controllers/basic-usage.js
@@ -1,42 +1,30 @@
-angular.module('restNgGrid.demo').controller('BasicUsage',['$scope', 'Resource', 'Http', 'DataModel','$q', '$uibModal', function($scope, Resource, Http, DataModel, $q, $uibModal) {
-  // putting our server data on scope to display it for learning purposes
-  $scope.dataModel = DataModel;
-  // trigger for loading groups
-  $scope.load = {groups: false};
-  $scope.dataGroups = angular.copy($scope.dataModel.dataGroups);
+angular.module('restNgGrid.demo').controller('BasicUsage', ['$scope', 'Resource', 'Http', 'DataModel', '$q', '$uibModal', function ($scope, Resource, Http, DataModel, $q, $uibModal) {
+    // putting our server data on scope to display it for learning purposes
+    $scope.dataModel = DataModel;
 
-  $scope.sourceParams = {groupId: null, productId: null};
-  
+    $scope.dataGroups = angular.copy($scope.dataModel.dataGroups);
 
-  //GRID OPTIONS
-  $scope.groupsGridOptions = {
-    //hierarchy: true,
-    // dataSource: {
-    //   type: "odata",
-    //   transport: {
-    //     get: "",
-    //     query: "/groups",
-    //     save: "/groups",
-    //     update: "/groups/{groupId}",
-    //     delete: "/groups/{groupId}"
-    //   },
-    //   //arrayListParam: '',
-    //   parentId: "",
-    //   itemId: "groupId"
-    // },
-    columns: {
-      titles: [
-        {
-          title: 'Reference'
+    //GRID OPTIONS
+    $scope.groupsGridOptions = {
+        columns: {
+            titles: [
+                {
+                    title: 'Name'
+                },
+                {
+                    title: 'Reference'
+                }
+
+            ],
+            fields: [
+                {
+                    field: 'name'
+                },
+                {
+                    field: 'reference'
+                }
+            ]
         }
-
-      ],
-      fields: [
-        {
-          field: 'name'
-        }
-      ]
-    }
-  };
+    };
 
 }]);
